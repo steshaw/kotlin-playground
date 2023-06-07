6d2004ab6d45abf3cc21d870f6a7478d7bb25eb0
Steven Shaw <steven@steshaw.org>
2023-06-07 15:30:34 +1000

    Ident check

---
+++ examples/ident.main.kts
@@ -0,0 +1,17 @@
+fun isValidIdentifier(s: String): Boolean {
+    if (s == "") {
+        return false
+    }
+    val first = s[0]
+    if (!first.isLetter() && first != '_') {
+        return false
+    }
+    return s.all{c -> c.isLetter() || c.isDigit() || c == '_'}
+}
+
+println(isValidIdentifier("name"))   // true
+println(isValidIdentifier("_name"))  // true
+println(isValidIdentifier("_12"))    // true
+println(isValidIdentifier(""))       // false
+println(isValidIdentifier("012"))    // false
+println(isValidIdentifier("no$"))    // false
