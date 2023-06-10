a1fa0406fd460705eefb1d61309a9a2412c9ce8a
Steven Shaw <steven@steshaw.org>
2023-06-10 15:06:52 +1000

    Expr

---
+++ src/main/kotlin/Expr.kt
@@ -0,0 +1,14 @@
+sealed interface Expr
+data class Constant(val i: Int) : Expr
+data class Add(val left: Expr, val right: Expr) : Expr
+
+fun eval(expr: Expr): Int =
+    when (expr) {
+        is Constant -> expr.i
+        is Add -> eval(expr.left) + eval(expr.right)
+    }
+
+fun main() {
+    val expr = Add(Constant(1), Add(Constant(20), Constant(21)))
+    println("${expr} = ${eval(expr)}")
+}
