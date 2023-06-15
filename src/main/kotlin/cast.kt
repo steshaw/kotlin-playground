54474695ce24965a58286fa53bf79ceaeff315af
Steven Shaw <steven@steshaw.org>
2023-06-15 18:32:36 +1000

    Cast

---
+++ src/main/kotlin/cast.kt
@@ -0,0 +1,14 @@
+fun lengthWhenString(a: Any?) : Int {
+    if (a is String) {
+        val s = a as String
+        return s.length
+    }
+    return 0
+}
+
+fun main() {
+    println(lengthWhenString(1))
+    println(lengthWhenString(null))
+    println(lengthWhenString(""))
+    println(lengthWhenString("abc"))
+}
