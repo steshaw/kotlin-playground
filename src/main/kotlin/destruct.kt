e79c5a94e2271c50c7af86bf952d63f48fe09488
Steven Shaw <steven@steshaw.org>
2023-06-15 15:51:24 +1000

    Destructuring lists :-o

--- src/main/kotlin/destruct.kt
+++ src/main/kotlin/destruct.kt
@@ -1,8 +1,8 @@
 data class Foo(val a: String, val b: String, val c: String)
 
-// It breaks the "law of least surprise" when destructuring with less than the correct number of components
-// is not rejected by the compiler.
-fun main() {
+// It breaks the "law of least surprise" that when destructuring with less than
+// the correct number of components is not rejected by the compiler.
+fun structs() {
     val f = Foo("a", "b", "c")
     println("f = $f")
 
@@ -14,4 +14,25 @@ fun main() {
 
     val (a2, b2, c2) = Foo("a", "b", "c")
     println("a2 = $a2, b2 = $b2, c2 = $c2")
+}
+
+// It breaks the "law of least surprise" that you can destructure lists this way at all.
+fun lists() {
+    val (a2) = listOf("a", "b")
+    println("a2 = $a2") // Nuts
+
+    val (a1, b1) = listOf("a", "b")
+    println("a1 = $a1, b1 = $b1") // Nuts
+
+    try {
+        val (a3, b3, c3) = listOf("a", "b")
+        println("a3 = $a3, b3 = $b3, c3 = $c3") // Explodes!
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        println("Out of bounds! $e")
+    }
+}
+
+fun main() {
+    structs()
+    lists()
 }
