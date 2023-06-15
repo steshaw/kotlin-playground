5d066f99f2b28de98ff05a5d8f7838ff804810ac
Steven Shaw <steven@steshaw.org>
2023-06-15 18:57:24 +1000

    Lambdas

---
+++ src/main/kotlin/lambda.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val m = mapOf(
+        1 to "one",
+        2 to "two",
+    )
+    println(m.mapValues { entry -> "[[${entry.key}: ${entry.value}]]" })
+    println(m.mapValues { (k, v) -> "[[$k: $v]]" })
+}
