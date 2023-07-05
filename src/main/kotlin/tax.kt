1968fd39dea0735ebbb0927f1083dcc7688022b5
Steven Shaw <steven@steshaw.org>
2023-07-05 13:52:05 +1000

    Refactor

--- src/main/kotlin/tax.kt
+++ src/main/kotlin/tax.kt
@@ -1,4 +1,3 @@
-import kotlin.IllegalArgumentException
 import kotlin.math.floor
 import kotlin.math.round
 
@@ -24,15 +23,6 @@ fun mkTable(): List<Tax> {
 
 val scale2Table = mkTable()
 
-fun VLOOKUP(income: Double, table: List<Tax>, position: Int): Double {
-    val tax = getTax(income, table)
-    return when (position) {
-        2 -> tax.multiplier
-        3 -> tax.base
-        else -> throw IllegalArgumentException("Got position $position")
-    }
-}
-
 private fun getTax(income: Double, table: List<Tax>) =
     table.reversed().find { income > it.income }!!
 
@@ -47,22 +37,19 @@ fun endsWith33Cents(d: Double): Boolean {
 private fun getAdjustedMonthlyIncome(monthlyIncome: Double) =
     monthlyIncome + if (endsWith33Cents(monthlyIncome)) 0.01 else 0.0
 
-fun getMultiplier(a: Double) = getTax(a, scale2Table).multiplier
-fun getBase(a: Double) = getTax(a, scale2Table).base
-
 fun monthlyWithholding(monthlyIncome: Double): Double {
     val adjustedMonthlyIncome = getAdjustedMonthlyIncome(monthlyIncome)
     println("adjustedMonthlyIncome = $adjustedMonthlyIncome")
     val weeklyIncome = floor((3.0 / 13.0) * adjustedMonthlyIncome)
     println("weeklyIncome = $weeklyIncome")
-    val multiplier = VLOOKUP(weeklyIncome, scale2Table, 2)
-    val base = VLOOKUP(weeklyIncome, scale2Table, 3)
-    return round(round((weeklyIncome + 0.99) * multiplier - base) * (13.0 / 3.0))
+    val tax = getTax(weeklyIncome, scale2Table)
+    println("tax = $tax")
+    return round(round((weeklyIncome + 0.99) * tax.multiplier - tax.base) * (13.0 / 3.0))
 }
 
 fun main() {
-    println(VLOOKUP(2019.00, scale2Table, 2))
-    println(VLOOKUP(2019.00, scale2Table, 3))
+    val tax = getTax(2019.00, scale2Table)
+    println(tax)
 
     fun p(monthlyIncome: Double) {
         val monthlyWithholding = monthlyWithholding(monthlyIncome)
