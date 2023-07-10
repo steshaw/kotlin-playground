9be8caf8623a6d059e7f18b484949ffd4512ab30
Steven Shaw <steven@steshaw.org>
2023-07-10 20:32:12 +1000

    Yay

--- src/main/kotlin/types.kt
+++ src/main/kotlin/types.kt
@@ -1,15 +1,32 @@
 fun main() {
-    fun fail(msg: String) {
-        throw Exception(msg)
-    }
+    run {
+        fun fail(msg: String) {
+            throw Exception(msg)
+        }
 
-    val i = 99
-    // Thankfully there is a warning here as result is inferred to be Any.
-    val result = if (i >= 42) {
-        42
-    } else {
-        fail("oops")
-    }
+        val i = 99
+        // Thankfully there is a warning here as result is inferred to be Any.
+        val result = if (i >= 42) {
+            42
+        } else {
+            fail("oops")
+        }
 
-    println(result)
+        println(result)
+    }
+    run {
+        fun fail(msg: String): Nothing {
+            throw Exception(msg)
+        }
+
+        val i = 99
+        // Yah, now result is inferred to be Int!
+        val result = if (i >= 42) {
+            42
+        } else {
+            fail("oops")
+        }
+
+        println(result)
+    }
 }
